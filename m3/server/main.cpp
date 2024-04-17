160c93e917b1892919c13e65fc1dbcf496f85eca
basilysf1709 <basilyusuf1709@gmail.com>
2024-04-17 13:21:12 -0400

    final changes

--- m3/server/main.cpp
+++ m3/server/main.cpp
@@ -10,13 +10,19 @@ int main()
                            .build();
 
     // Decorate the TCPServer with the SecurityServerDecorator
+    // Decorator Pattern
     SecurityServerDecorator secureServer(&server);
 
+    // Observer Pattern & Singleton Pattern
     EventLogger logger;
+
+    // Observer attached
     server.attach(&logger);
 
     // Start the server
     secureServer.start();
+
+    // Observer detached
     server.detach(&logger);
 
     // When main exits, the TCPServer instance (server) will be automatically destroyed.
